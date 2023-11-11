91a8e31f722d7f5cf02f895228bd2cdd4453ad88
harshsbajwa <harsh.bajwa@mail.utoronto.ca>
2023-11-11 18:37:40 -0500

    made apps, did most of applications? made changes to comments, put temp files for pet_listing and shelter

--- comments/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [ 
-    path('user/comments/<int:pk>/shelter', views.ShelterCommentsListCreate.as_view()),
-     path('user/comments/<int:pk>/application', views.ApplicationCommentsListCreate.as_view()),
-]
