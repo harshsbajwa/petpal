641c4270baf34b4c9e9d018ed05b710aa3f37ebb
harshsbajwa <harsh.bajwa@mail.utoronto.ca>
2023-12-12 14:00:10 -0500

    q. 1.

--- backend/shelterblog/urls.py
+++ backend/shelterblog/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name="shelterblog"
 urlpatterns = [ 
     path('shelter/<int:pk1>/blog/', views.ListShelterBlogPost.as_view()),
+    path('shelter/<int:pk1>/blog/create/', views.CreateShelterBlogPost.as_view()),
     path('shelter/<int:pk1>/blog/<int:pk>/', views.RetrieveShelterBlogPost.as_view()),
     path('shelter/<int:pk1>/blog/<int:pk>/edit/', views.UpdateShelterBlogPost.as_view()),
     path('shelter/<int:pk1>/blog/<int:pk>/delete/', views.DestroyAPIView.as_view()),
